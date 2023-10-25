1b972f0832ada727a9985d29394e6d390f865bdf
rafam3n1 <tornevirtual@gmail.com>
2023-10-25 20:29:11 -0300

    recomeçando

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-const { app, BrowserWindow, BrowserView, Tray, Menu, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 let mainWindow;
 let view;
-let tray;
-let floatingButton;
-let loadingElement;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: __dirname + '/preload.js',
       nodeIntegration: true,
       contextIsolation: false
     }
@@ -20,70 +16,40 @@ function createWindow() {
   mainWindow.maximize();
 
   // Criando e configurando o BrowserView
-  view = new BrowserView();
+  view = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
   mainWindow.setBrowserView(view);
   view.setBounds({ x: 0, y: 0, width: mainWindow.getBounds().width, height: mainWindow.getBounds().height });
-  view.webContents.loadURL('https://grupobright.com/dashboard/');
+  view.webContents.loadURL('https://grupobright.com/wp-admin/');
 
-  // Criando o botão flutuante como uma janela separada
-  floatingButton = new BrowserWindow({
-    width: 50,
-    height: 50,
-    frame: false,
-    transparent: true,
-    alwaysOnTop: true,
-    resizable: false,
-    skipTaskbar: true,
-    show: false
-  });
-  floatingButton.loadFile('floating_button_html.html');
-
-  // Criando o elemento de carregamento como uma janela separada
-  loadingElement = new BrowserWindow({
-    width: 100,
-    height: 100,
-    frame: false,
-    transparent: true,
-    alwaysOnTop: true,
-    resizable: false,
-    skipTaskbar: true,
-    show: false
-  });
-  loadingElement.loadFile('loading_html.html');
-
-  Menu.setApplicationMenu(null);
-}
-
-function createTray() {
-  tray = new Tray('./img/LocalLogo.ico');
-  tray.setToolTip('Bright App');
-
-  const contextMenu = Menu.buildFromTemplate([
+  // Criando o menu
+  const menuTemplate = [
     {
-      label: 'Abrir Bright App', click: function() {
-        mainWindow.show();
-        mainWindow.maximize();
-      }
-    },
-    {
-      label: 'Fechar Bright App', click: function() {
-        app.quit();
-      }
+      label: 'Menu',
+      submenu: [
+        {
+          label: 'Home',
+          click: () => {
+            view.webContents.loadURL('https://grupobright.com/wp-admin/');
+          }
+        },
+        {
+          label: 'Conta',
+          click: () => {
+            view.webContents.loadURL('https://grupobright.com/minha-conta/');
+          }
+        }
+      ]
     }
-  ]);
-  tray.setContextMenu(contextMenu);
+  ];
 
-  tray.on('double-click', function() {
-    mainWindow.show();
-    mainWindow.maximize();
-  });
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 }
-
-app.on('browser-window-blur', function() {
-  if (!mainWindow.isVisible()) {
-    createTray();
-  }
-});
 
 app.whenReady().then(createWindow);
 
@@ -97,12 +63,4 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
-});
-
-ipcMain.on('navigate', (event, url) => {
-  view.webContents.loadURL(url);
-});
-
-ipcMain.on('navigate-back', (event) => {
-  view.webContents.goBack();
 });
--- preload.js
+++ preload.js
@@ -14,4 +14,4 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
-})
+});
