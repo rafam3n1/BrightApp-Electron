2ff27c93c19ce390d62d04d59bcac5519ef9354d
rafam3n1 <tornevirtual@gmail.com>
2023-10-25 21:28:09 -0300

    loading

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, BrowserView, Menu } = require('electron');
 
 let mainWindow;
 let view;
+let loadingWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + '/img/LocalLogo - Copia.png', // Adicione esta linha
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -16,17 +18,23 @@ function createWindow() {
   mainWindow.maximize();
 
   // Criando e configurando o BrowserView
-  view = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
+  view = new BrowserView();
   mainWindow.setBrowserView(view);
   view.setBounds({ x: 0, y: 0, width: mainWindow.getBounds().width, height: mainWindow.getBounds().height });
   view.webContents.loadURL('https://grupobright.com/wp-admin/');
 
-  // Criando o menu
+  view.webContents.on('did-start-loading', () => {
+    console.log("Iniciou o carregamento");
+    loadingWindow.show();
+
+    // Oculta a janela de loading após 5 segundos
+    setTimeout(() => {
+      loadingWindow.hide();
+    }, 5000);
+  });
+
+  createLoadingWindow();
+
   const menuTemplate = [
     {
       label: 'Menu',
@@ -49,6 +57,20 @@ function createWindow() {
 
   const menu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(menu);
+}
+
+function createLoadingWindow() {
+  loadingWindow = new BrowserWindow({
+    width: 200,
+    height: 200,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    resizable: false,
+    parent: mainWindow, // Define a mainWindow como janela pai
+    show: false
+  });
+  loadingWindow.loadFile('loading.html');
 }
 
 app.whenReady().then(createWindow);
